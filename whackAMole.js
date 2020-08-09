546989c50daabf9f0a7adb7e2df40255966c6db6
Tom Tyler <tom.tyler@live.co.uk>
2020-08-09 21:59:07 +0100

    first commit

---
+++ whackAMole.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    //create variables    
+    const square = document.querySelectorAll(".square");
+    const mole = document.querySelectorAll(".mole");
+    const timeLeft = document.querySelector("#time-left");
+    let score = document.querySelector("#score");
+    let currentTime = timeLeft.textContent;
+    let result = 0;
+    var playing = false;
+    const start = document.querySelector("#start");
+    const reset = document.querySelector("#reset");
+    let hitPosition = null;
+    //start the game when the player commands
+    start.addEventListener("click", function(){
+        this.style.display = "none";
+        reset.style.display = "block";
+        playing = true;
+        moveMole();
+        //begin the countdown timer
+        setInterval(countDown, 1000);
+        
+        function countDown(){
+            if(playing === true){
+                currentTime--;
+                timeLeft.textContent = currentTime;
+                //when the time runs out stop the timer and tell the player their score
+                if(currentTime === 0){
+                    playing = false;
+                    hitPosition = null;
+                    clearInterval(countDown);
+                    clearInterval(randomSquare, 750);
+                    removeMoles();
+                    reset.textContent = "Play Again";
+                    alert("GAME OVER - Final Score: " + result);
+                }
+            }
+        }
+    })
+    //reset the game when the player commands
+    reset.addEventListener("click", function(){
+        location = location;
+    })
+    //check if a mole has been whacked
+    square.forEach(function(item){
+        item.addEventListener("mouseup", () =>{
+            if(item.id === hitPosition){
+                result = result + 1;
+                score.textContent = result;
+            }
+        })
+    })
+    //make the mole appear on a random square
+    function randomSquare(){
+        if(playing === true){
+            removeMoles();
+            let randomPosition = square[Math.floor(Math.random() * 9)];
+            randomPosition.classList.add("mole");
+            hitPosition = randomPosition.id;
+        }    
+    }
+    //move the mole to a random sqaure
+    function moveMole(){
+            setInterval(randomSquare, 750);
+    }
+    //remove the mole from the gameboard and 
+    function removeMoles(){
+        square.forEach(className =>{
+            className.classList.remove("mole");
+        });
+    }
+    //animate the hammer hitting when a square is clicked
+    square.forEach(function(item){
+        item.addEventListener("mousedown", function(){
+            this.classList.add("hammerDown");
+        });
+        item.addEventListener("mouseup", function(){
+            this.classList.remove("hammerDown");
+        });
+    })
+
+});
